74a12cb4c632e0786ebd23063ef254f5b032fec6
sendweb <sendweb@163.com>
2022-04-02 21:51:07 +0800

    增加路径，使得tomcat和直接运行的路径一致： server.servlet.context-path=/zuulWar
    加上post+json的处理，序列化和反序列号都成功：getDog

--- src/main/java/abc/xhb/zuulwar/Dog.java
+++ src/main/java/abc/xhb/zuulwar/Dog.java
@@ -1,31 +1,36 @@
 package abc.xhb.zuulwar;
 
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class Dog {
     private String name;
     private String age;
 
-    public Dog() {
-    }
-
-    public Dog(String name, String age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getAge() {
-        return age;
-    }
+//    public Dog() {
+//    }
+//
+//    public Dog(String name, String age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setAge(String age) {
+//        this.age = age;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String getAge() {
+//        return age;
+//    }
 }
--- src/main/java/abc/xhb/zuulwar/HelloController.java
+++ src/main/java/abc/xhb/zuulwar/HelloController.java
@@ -2,6 +2,8 @@ package abc.xhb.zuulwar;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,11 +21,19 @@ public class HelloController {
         return person;
     }
 
+    @ResponseBody
+    @PostMapping("getDog")
+    public Dog getDog(@RequestBody Dog dog) {
+        dog.setName("你的狗是：" + dog.getName());
+        return dog;
+    }
+
+
     //@ResponseBody
     @RequestMapping("/test")
     public void sayHello(HttpServletRequest request, HttpServletResponse response) {
         System.out.println(request.getQueryString());
-        response.setHeader("refresh","5;url=https://www.baidu.com");
+        response.setHeader("refresh", "5;url=https://www.baidu.com");
 
     }
 }
