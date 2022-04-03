b0fcc5edb5d8fc76820c9cb9df5f574d882f843f
sendweb <sendweb@163.com>
2022-04-03 11:15:28 +0800

    加入zuulFilter，并且@ConditionalOnProperty(value = "Bean.PreFilter", havingValue = "true")

--- src/main/java/abc/xhb/zuulwar/HelloController.java
+++ src/main/java/abc/xhb/zuulwar/HelloController.java
@@ -1,7 +1,9 @@
 package abc.xhb.zuulwar;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +14,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
-@AllArgsConstructor
+//@AllArgsConstructor
+@ConditionalOnProperty(value = "Bean.Controller", havingValue = "true")
+@RequiredArgsConstructor
 public class HelloController {
     private final Person person;
 
---
+++ src/main/java/abc/xhb/zuulwar/MyZuulFilter.java
@@ -0,0 +1,55 @@
+package abc.xhb.zuulwar;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@ConditionalOnProperty(value = "Bean.PreFilter", havingValue = "true")
+public class MyZuulFilter extends ZuulFilter {
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        ctx.addZuulResponseHeader("Content-type", "text/json;charset=UTF-8");
+        ctx.getResponse().setCharacterEncoding("UTF-8");
+        System.out.println("请求地址:"+request.getRequestURI());
+        String token = request.getParameter("token");
+        String msg="请求成功!";
+        if(token==null) {
+            ctx.setSendZuulResponse(false);
+            msg="请求失败！";
+            ctx.setResponseBody(msg);
+            ctx.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+        }
+        return msg;
+    }
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Bean
+    public MyZuulFilter zuulFilter() {
+        return new MyZuulFilter();
+    }
+}
